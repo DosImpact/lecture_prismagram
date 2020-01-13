e556fa1e6de151d003770e2ce3049c8df9ab3eda
DosImpact <ypd03008@gmail.com>
2020-01-13 18:42:31 +0900

    done #3.3

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { sendSecretMail } from "./utils";
 
+sendSecretMail("ypd03008@gmail.com", "123");
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
@@ -13,7 +17,7 @@ server.start({ port: PORT }, () =>
 );
 
 /**
- * 설정파일에서 포트번호를 불러오거나 또는 4000으로 설정한다.
+ * 설정파일에서 포트번호를 불러오거나 또는 4000으로 설정한다. path에서 __dirname 가져오고, .env 연결 시켜주기.
  * ( <1>&&<2>&&<3>&& 의 논리는 1이 만족되고 2도 만족된다면 3도 가즈아.. 반대의 논리면, 1아니면, 2로 하면되지. 뭐든.. )
  * GraphQLServer를 스키마를 넣고 실행시킨다.
  * express서버를 가져와서 morgan의 logger를 사용해서 실행시킨다.
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,35 @@
 import { adjectives, nouns } from "./words";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "ehdudtkatka@prismagram.com",
+    to: adress,
+    subject: "🔒Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+  };
+  return sendMail(email);
+};
 /**
  * words에서 단어들을 가져와서, 0~1랜덤에다 단어길이 곱하면 랜덤 인덱스가 나옴. ->`` 을 통해 문자열을 꾸며준다.
  */
