a9c802b008827b91e23fff52797b685aa8761ed7
DosImpact <ypd03008@gmail.com>
2020-01-13 21:34:24 +0900

    #3.6

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,5 @@
 import { adjectives, nouns } from "./words";
 
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
 import jwt from "jsonwebtoken";
