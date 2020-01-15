31f5a8fa1ac16e71acf216d4a589a613a9d4d885
DosImpact <ypd03008@gmail.com>
2020-01-15 19:56:09 +0900

    #3.18

--- README.md
+++ README.md
@@ -210,7 +210,39 @@ export default {
 
 # 3.16 itsMe and amIFollowing Fileds part Two (8:49)
 
+### computed field fullName: String amIFollowing: Boolean! itsMe: Boolean! 추가하기.
+
 # 3.17 isLiked Computed File (4:51)
+
+### 내가 해당 포스트에 좋아요 눌렀는지 확인.
+
+```js
+Post: {
+  //낯설다.: rule: Class적인 변수의 개념과, inst적인 변수의 개념이 있다.
+  // parent는 어떠한 Post의 인스턴스고, request의 있는 user도 마찬가지.
+  // prisma.은 class적인 느낌으로, 모든 inst에서 찾는다.
+  isLiked: (parent, _, { request }) => {
+    const { user } = request; //현재 접속중인 유저
+    const { id } = parent; //어떤 포스트
+    //fb) AND가 아닌 OR의 논리로 생각했다. = 나는 어떤 좋아요를 누른적이 있다. , 어떤 좋아요는 Post에 달렸다.
+    // 어떤 좋아요 = {사용자 = 나 }(&& 이고) { 해당 포스트에 되있다면.. }
+    return prisma.$exists.like({
+      AND: [
+        {
+          user: {
+            id: user.id
+          }
+        },
+        {
+          post: {
+            id
+          }
+        }
+      ]
+    });
+  };
+}
+```
 
 # 3.18 upload Resolver (12:26)
 
--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -9,28 +9,55 @@ export default {
       //console.log(parent);
       return `${parent.firstName} ${parent.lastName}`;
     },
-    amIFollowing: async (parent, _, { request }) => {
+    isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       try {
-        const exists = await prisma.$exists.user({
-          AND: [{ id: parentId }, { followers_some: [user.id] }]
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              following_some: {
+                id: parentId
+              }
+            }
+          ]
         });
-        console.log(exists);
-        if (exists) {
-          return true;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
+      } catch {
         return false;
       }
     },
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
+    }
+  },
+  Post: {
+    //낯설다.: rule: Class적인 변수의 개념과, inst적인 변수의 개념이 있다.
+    // parent는 어떠한 Post의 인스턴스고, request의 있는 user도 마찬가지.
+    // prisma.은 class적인 느낌으로, 모든 inst에서 찾는다.
+    isLiked: (parent, _, { request }) => {
+      const { user } = request; //현재 접속중인 유저
+      const { id } = parent; //어떤 포스트
+      //fb) AND가 아닌 OR의 논리로 생각했다. = 나는 어떤 좋아요를 누른적이 있다. , 어떤 좋아요는 Post에 달렸다.
+      // 어떤 좋아요 = {사용자 = 나 }(&& 이고) { 해당 포스트에 되있다면.. }
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id
+            }
+          }
+        ]
+      });
     }
   }
 };
