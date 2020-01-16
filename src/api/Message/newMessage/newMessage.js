9d60f8b01196cd60c0063be8d563600222f5bc11
DosImpact <ypd03008@gmail.com>
2020-01-16 16:31:28 +0900

    #3.29 done backend!!

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -6,16 +6,18 @@ export default {
       //기존의 쿼리,mutation과 다르게, subscribe( 조건 ),resolve(결과 가공 전달) 을 정의 해야한다.
       subscribe: (_, args) => {
         const { roomId } = args;
-        return prisma.$subscribe.message({
-          AND: [
-            { mutation_in: "CREATED" },
-            {
-              node: {
-                room: { id: roomId }
+        return prisma.$subscribe
+          .message({
+            AND: [
+              { mutation_in: "CREATED" },
+              {
+                node: {
+                  room: { id: roomId }
+                }
               }
-            }
-          ]
-        });
+            ]
+          })
+          .node();
       },
       //
       resolve: payload => payload
