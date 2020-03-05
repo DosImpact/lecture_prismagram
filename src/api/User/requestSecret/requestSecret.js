77b8e5ce53bf33bed325df86a4da90fd94d8eaef
DosImpact <ypd03008@gmail.com>
2020-03-05 21:12:25 +0900

    requestSecret

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -8,7 +8,7 @@ export default {
       const { email } = args;
       const loginSecret = generateSecret();
       try {
-        //await sendSecretMail(email, loginSecret);
+        await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
       } catch (error) {
